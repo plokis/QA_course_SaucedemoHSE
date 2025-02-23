f6ac6a57f96d4a2ee7335cad4d64b47b6fdfe13e
plokis <167650528+plokis@users.noreply.github.com>
2025-02-23 20:22:13 +0300

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+public class LoginTest {
+
+    /*
+    1. Открыть страницу https://www.saucedemo.com/
+    2. Ввести в поле username значение dqdq
+    3. Оставить поле password пустым dqdq
+    4. Нажать кнопку Login
+    5. Проверить, что мы видим сообщение об ошибке с текстом
+       Epic sadface: Username and password do not match any user in this service
+     */
+
+    @Test
+    public void checkNegativeLoginWithInvalidUsernameAndPasswordByMessage() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("dqdq");
+        driver.findElement(By.id("password")).sendKeys("dqdq");
+        driver.findElement(By.id("login-button")).click();
+
+        String errorMessage = driver.findElement(By.cssSelector("[data-test=error]")).getText();
+
+        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service");
+
+        driver.quit();
+    }
+
+    /*
+    1. Открыть страницу https://www.saucedemo.com/
+    2. Ввести в поле username значение dqdq
+    3. Оставить поле password пустым dqdq
+    4. Нажать кнопку Login
+    5. Проверить, что мы видим круглую красную иконку с белым крестиком
+    */
+
+    @Test
+    public void checkNegativeLoginWithInvalidUsernameAndPasswordByIcon() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("dqdq");
+        driver.findElement(By.id("password")).sendKeys("dqdq");
+        driver.findElement(By.id("login-button")).click();
+
+        boolean iconIsVisible = driver.findElement(By.className("fa-times-circle")).isDisplayed();
+
+        Assert.assertTrue(iconIsVisible);
+
+        driver.quit();
+    }
+}
